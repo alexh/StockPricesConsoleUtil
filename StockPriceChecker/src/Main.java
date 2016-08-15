feae60ee8363afd40503e7bc70a3b91ab8cb2a1f
alexhaynes32 <alhaynes@seas.upenn.edu>
2016-08-15 17:31:30 -0500

    added output formatting and user input functions

--- StockPriceChecker/src/Main.java
+++ StockPriceChecker/src/Main.java
@@ -1,6 +1,10 @@
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Scanner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,28 +20,31 @@ public class Main {
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
 	
-	public static void main(String[] args) {
+	
+	private static void PrintStocks(String file){
 		String baseURL = "http://finance.google.com/finance/info?client=ig&q=";
-		
 		List<String> stocks = null;
 		try {
-			stocks = WatchlistReader.ReadWatchlist(args[0]);
+			stocks = WatchlistReader.ReadWatchlist(file);
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		System.out.print(ANSI_WHITE);
+		System.out.println("| Ticker  | Price \t | Change \t | Percent Change | Time \t\t|");
 		for(String stock : stocks){
+			System.out.println("---------------------------------------------------------------------------------");
 			 JSONObject json = null;
 				try {
 					json = GoogleFinanceAPIJsonReader.readJsonFromUrl(baseURL + stock);
 				} catch (JSONException e) {
-					// TODO Auto-generated catch block
+					System.out.println("Error in fetching data for " + stock + ". Did you spell it correctly?");
 					e.printStackTrace();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					System.out.println("Error reading watchlist. Make sure the file exists.");
 					e.printStackTrace();
 				}
-				   System.out.print(ANSI_WHITE);
+				   
 				    String ticker = json.getString("t");
 				    String price = json.getString("l");
 				    String change = json.getString("c");
@@ -51,21 +58,58 @@ public class Main {
 				    	changeType = -1;
 				    }
 				    
-				    System.out.println(ticker);
+				    System.out.print("| " + ticker + "\t");
 				    if (changeType == 1){
 				    	System.out.print(ANSI_GREEN);
 				    }
 				    if (changeType == -1){
 				    	System.out.print(ANSI_RED);
 				    }
-				    System.out.println(price);
-				    System.out.println(change);
-				    System.out.println(changePercent);
+				    
+				    System.out.print("| " + price + " \t ");
+				    System.out.print("| " + change + " \t ");
+				    System.out.print("| " + changePercent + "% \t  ");
 				    System.out.print(ANSI_WHITE);
-				    System.out.println(time);
+				    System.out.println("| " + time + " \t|");
 		}
 		
 		System.out.println(ANSI_RESET);
+	}
+	
+	public static void main(String[] args) {
+		
+		String input = "stocks";
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		while (!input.equals("quit")){
+			System.out.println("Type 'quit' to quit, Type 'add TICK' to add a stock, Type 'stocks' to see stock prices, Type 'stocks 10 20' to refresh prices every 10 seconds for 20 seconds");
+			input = reader.nextLine(); 
+			if (input.equals("stocks")){
+				PrintStocks(args[0]);
+			}
+			if (input.substring(0, 3).contains("add")){
+				try
+				{
+				    FileWriter fw = new FileWriter(args[0],true); //the true will append the new data
+				    fw.write("," + input.substring(4));//appends the string to the file
+				    fw.close();
+				}
+				catch(IOException ioe)
+				{
+				   System.out.println("Couldn't add");
+				}
+				System.out.println("Current Watchlist: ");
+				try {
+					List<String> stocks = WatchlistReader.ReadWatchlist(args[0]);
+					for (String s : stocks){
+						System.out.print(s + ",");
+					}
+				} catch (FileNotFoundException e) {
+				}
+				System.out.println("");
+			}
+		}
+		reader.close();
+		
 		
 	}
 
